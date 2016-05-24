9ec3331ea753f2204c4f63d873c0a76e9c5331a4
Kozhabay Alisher <alikdemon@gmail.com>
2016-05-25 01:22:50 +0600

    Added Toolbar and Search button

---
+++ ReminderApp/app/src/main/java/kz/alisher/reminder/MainActivity.java
@@ -0,0 +1,36 @@
+package kz.alisher.reminder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by Alisher Kozhabay on 25.05.2016.
+ */
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
+
+}
